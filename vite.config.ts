0f63d4f264be616d6e8ee075828cef38f5b2dc21
Flaneur <flaneur@FlaneurdeMacBook-Pro.local>
2023-05-06 16:31:38 +0800

    update logo

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,29 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Jazz Tutor",
+        short_name: "Jazz Tutor",
+        description: "Jazz Tutor",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
