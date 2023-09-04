c3835c17cc97f637fc69ca81dd21a83bf8db50a3
Flaneur <flaneur@FlaneurdeMacBook-Pro.local>
2023-09-04 16:30:36 +0800

    remove home

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { RecoilRoot } from "recoil";
 import { Menu } from "./components/Menu";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 const router = createBrowserRouter([
   {
     path: "/chord",
